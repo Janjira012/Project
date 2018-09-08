5641c30af3e8723882902ed98ffa3317f5ac65c5
janjira012 <s5830213012@phuket.psu.ac.th>
2018-09-08 22:25:55 +0700

    install  mustache

--- www/js/data.js
+++ www/js/data.js
@@ -2,18 +2,23 @@ function searchB() {
     
     var x, text, y;
     x = document.getElementById("searchAuthorsName").value;
-    y = document.getElementById("searchAuthorLastsname").value;
+    y = document.getElementById("searchAuthorLastName").value;
     z = document.getElementById("affiliation").value;
 
     var url = 'https://api.elsevier.com/content/search/author?query=authlast('+y+')%20and%20authfirst('+x+')%20and%20affil('+z+')&apiKey=7f59af901d2d86f78a1fd60c1bf9426a';
     console.log(url);
-   
+ 
     $.ajax({
         url: url ,
         type: 'GET'
     }).then(function (data) {
-       console.log(data);
-       
+        for (var i = 0; i < data.length; i++) {
+            for (var j = 0; j < data.length; j++) {
+        var row = data[i][j];
+        console.log(row);
+            }
+        }
+      
     });
 
     
