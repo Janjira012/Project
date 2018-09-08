70087a2596255892b8823f979fe9c92df1d39ee9
janjira012 <s5830213012@phuket.psu.ac.th>
2018-09-08 21:04:04 +0700

    edit js

--- www/js/data.js
+++ www/js/data.js
@@ -1,14 +1,19 @@
-
-
-$('#searchB').click(function(){
-    loadPostByName();
-});
-
-
-
-function loadGetByName() {
+function searchB() {
     
+    var x, text, y;
+    x = document.getElementById("searchAuthorsName").value;
+    y = document.getElementById("searchAuthorLastsname").value;
+    z = document.getElementById("affiliation").value;
+
+    var url = 'https://api.elsevier.com/content/search/author?query=authlast('+y+')%20and%20authfirst('+x+')%20and%20affil('+z+')&apiKey=7f59af901d2d86f78a1fd60c1bf9426a';
+    console.log(url);
+    $.getJSON("url", function(result){
+        text = 'Data: {$data.data}'
+        console.log(text);
+        
+    });
     
-    }
+
     
+}
 
