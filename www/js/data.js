9030e7c2becbfcad8a670ddea7809c5a038086f4
janjira012 <s5830213012@phuket.psu.ac.th>
2018-09-08 21:15:18 +0700

    edit js url

--- www/js/data.js
+++ www/js/data.js
@@ -7,11 +7,7 @@ function searchB() {
 
     var url = 'https://api.elsevier.com/content/search/author?query=authlast('+y+')%20and%20authfirst('+x+')%20and%20affil('+z+')&apiKey=7f59af901d2d86f78a1fd60c1bf9426a';
     console.log(url);
-    $.getJSON("url", function(result){
-        text = 'Data: {$data.data}'
-        console.log(text);
-        
-    });
+   
     
 
     
