1fe1c5d17a4817425dccd18a3475b04ae6b46ca4
janjira012 <s5830213012@phuket.psu.ac.th>
2018-09-08 21:24:43 +0700

    add console.log(data)

--- www/js/data.js
+++ www/js/data.js
@@ -8,7 +8,13 @@ function searchB() {
     var url = 'https://api.elsevier.com/content/search/author?query=authlast('+y+')%20and%20authfirst('+x+')%20and%20affil('+z+')&apiKey=7f59af901d2d86f78a1fd60c1bf9426a';
     console.log(url);
    
-    
+    $.ajax({
+        url: url ,
+        type: 'GET'
+    }).then(function (data) {
+       console.log(data);
+       
+    });
 
     
 }
