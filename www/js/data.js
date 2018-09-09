d66da8b08da031080d740930e142eb2b293fa302
janjira012 <s5830213012@phuket.psu.ac.th>
2018-09-09 22:33:41 +0700

    edit js

--- www/js/data.js
+++ www/js/data.js
@@ -6,26 +6,27 @@ function searchB() {
     z = document.getElementById("affiliation").value;
 
     var url = 'https://api.elsevier.com/content/search/author?query=authlast('+y+')%20and%20authfirst('+x+')%20and%20affil('+z+')&apiKey=7f59af901d2d86f78a1fd60c1bf9426a';
-    console.log(url);
+   // console.log(url);
  
     $.ajax({
         url: url ,
-        type: 'GET'
+        type: 'GET',
+        dataType :  'json',
+        //success : onSuccess,
+       //error : onError
+        
     }).then(function (data) {
+      // console.log(url);
        //console.log(data);
-        
-        
-        for (var i = 0; i < data.length; i++) {
-            // for (var j = 0; j < data.length; j++) {
-                // for (var k = 0; k < data.length; k++) {
-            
-        var row = data.search-result.entry[0].eid;
 
-       console.log(data[0]);
-        //$('#show').append(row);
-            // }
-        // }
-    }
+        
+        
+        var row = data.search-results.entry.eid;
+       console.log(row);
+       
+    
+   
+
     
     });
 
