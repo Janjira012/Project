0cd68c2812328884a4006685b84732a2574c9f2b
janjira012 <s5830213012@phuket.psu.ac.th>
2018-09-11 10:22:48 +0700

    edit data js

--- www/js/data.js
+++ www/js/data.js
@@ -17,7 +17,7 @@ function searchB() {
        success : function (data) {
            var x = data
           //console.log(data.search-results);
-          console.log(x['search-results'].entry.preferred-name);
+          console.log(x['search-results'].entry[0]['preferred-name']);
 
           //window.location.href = "reult.htm?id=" ;
        }
